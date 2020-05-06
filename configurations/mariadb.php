ecb1dfa71ed9bd9cacd95255cae97fa18c0fe0a0
alex <alexsey_89@bk.ru>
2020-05-07 00:08:09 +0300

    added configuration for mariadb

---
+++ configurations/mariadb.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'connection' => new PDO('mysql:dbname='.getenv('DB_NAME').';
+                           host='.getenv('DB_HOST').';
+                           port='.getenv('DB_PORT'),
+               getenv('DB_USER'), getenv('DB_PASSWORD'), [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+])];
