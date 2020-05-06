316bfb28388bfeafc821cfe9f27842205d45af28
alex <alexsey_89@bk.ru>
2020-05-07 00:07:29 +0300

    include in composer php-di and laminas-config-aggregator

--- public/index.php
+++ public/index.php
@@ -2,6 +2,27 @@
 
 declare(strict_types=1);
 
+use DI\ContainerBuilder;
+use Laminas\ConfigAggregator\ConfigAggregator;
+use Laminas\ConfigAggregator\PhpFileProvider;
+
 http_response_code(500);
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$builder = new ContainerBuilder();
+
+// агрегируем все настройки
+$aggregator = new ConfigAggregator([
+    new PhpFileProvider(__DIR__ . '/../configurations/*.php')
+]);
+
+
+$builder->addDefinitions($aggregator->getMergedConfig());
+
+try {
+    $container = $builder->build();
+} catch (Exception $e) {
+}
+
+var_dump($container->get('connection'));
