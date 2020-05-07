dc8292aa4be70073fba75aeebc42a5d773b8becb
alex <alexsey_89@bk.ru>
2020-05-07 23:55:17 +0300

    simplified di container

---
+++ configurations/container.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use DI\ContainerBuilder;
+use Laminas\ConfigAggregator\ConfigAggregator;
+use Laminas\ConfigAggregator\PhpFileProvider;
+
+$builder = new ContainerBuilder();
+
+$aggregator = new ConfigAggregator([
+    new PhpFileProvider(__DIR__ . '/dev/*.php')
+]);
+
+$builder->addDefinitions($aggregator->getMergedConfig());
+
+try {
+    return $builder->build();
+} catch (Exception $e) {
+
+}
--- public/index.php
+++ public/index.php
@@ -2,27 +2,10 @@
 
 declare(strict_types=1);
 
-use DI\ContainerBuilder;
-use Laminas\ConfigAggregator\ConfigAggregator;
-use Laminas\ConfigAggregator\PhpFileProvider;
-
 http_response_code(500);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$builder = new ContainerBuilder();
+$container = require __DIR__ . '/../configurations/container.php';
 
-// агрегируем все настройки
-$aggregator = new ConfigAggregator([
-    new PhpFileProvider(__DIR__ . '/../configurations/*.php')
-]);
-
-
-$builder->addDefinitions($aggregator->getMergedConfig());
-
-try {
-    $container = $builder->build();
-} catch (Exception $e) {
-}
-
-var_dump($container->get('connection'));
+var_dump($container->get('configuration')['mariadb']['connection']);
